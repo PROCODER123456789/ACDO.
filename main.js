2567d751d7d388996df83009a2b7e5f68b5e4c2f
PROCODER123456789 <99879654+PROCODER123456789@users.noreply.github.com>
2022-10-11 10:28:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function ChemiMastermind(){
+    console.log("CM program clicked !")
+}
+
+function basic(){
+    console.log("basics program clicked !")
+}
+
+function Intermediate(){
+    console.log("Intermediate program clicked !")
+}
+
+function Advanced(){
+    console.log("advanced program clicked !")
+}
